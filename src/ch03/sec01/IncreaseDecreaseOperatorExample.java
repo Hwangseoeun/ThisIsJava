8313db3e14fc18da77f757df53d3d5bb376898a3
Hwangseoeun <sunny030921@gmail.com>
2023-05-25 19:31:51 +0900

    IncreaseDecreaseOperatorExample

---
+++ src/ch03/sec01/IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,11 @@
+package ch03.sec01;
+
+public class IncreaseDecreaseOperatorExample {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 10;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
